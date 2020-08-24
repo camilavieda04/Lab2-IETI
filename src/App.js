e99c5d8721c17f7b65bfec992caf3dbc302ab850
camilavieda04 <camila_vieda04@hotmail.com>
2020-08-23 20:31:48 -0500

    Parte 2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
-import {TodoList} from "./TodoList";
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
 import moment from "moment";
+import {Login} from "./components/Login";
+import {TodoApp} from "./TodoApp";
+import {BrowserRouter as Router, Link, Route} from 'react-router-dom';
+
 
 class App extends Component {
 
@@ -16,61 +19,46 @@ class App extends Component {
         this.handleDateChange = this.handleDateChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
     }
-
+    
 
     render() {
+        const LoginView = () => (
+            <Login  />
+        );
+
+        const TodoAppView = () => (
+            <TodoApp />
+        );
+
+        this.state = {
+            LoginView: LoginView,
+            TodoAppView : TodoAppView,
+            isLoggedIn: false
+        };
 
         return (
-            <div className="App">
-                <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo"/>
-                    <h1 className="App-title">TODO React App</h1>
-                </header>
 
-                <br/>
-                <br/>
-                <form onSubmit={this.handleSubmit} className="todo-form">
-                    <h3>New TODO</h3>
-                    <label htmlFor="text" className="right-margin">
-                        Text:
-                    </label>
-
-                    <input
-                        id="text"
-                        onChange={this.handleTextChange}
-                        value={this.state.text}>
-                    </input>
+            <Router>
+                <div className="App">
+                    <header className="App-header">
+                        <img src={logo} className="App-logo" alt="logo"/>
+                        <h1 className="App-title">TODO React App</h1>
+                    </header>
 
                     <br/>
                     <br/>
-                    <label htmlFor="priority" className="right-margin">
-                        Priority:
-                    </label>
 
-                    <input
-                        id="priority"
-                        type="number"
-                        onChange={this.handlePriorityChange}
-                        value={this.state.priority}>
-                    </input>
-                    <br/>
-                    <br/>
+                    <ul>
+                        <li><Link to="/">Login</Link></li>
+                        <li><Link to="/todo">Todo</Link></li>
+                    </ul>
 
-                    <DatePicker
-                        id="due-date"
-                        selected={this.state.dueDate}
-                        placeholderText="Due date"
-                        onChange={this.handleDateChange}>
-                    </DatePicker>
-                    <br/>
-                    <button>
-                        Add #{this.state.items.length + 1}
-                    </button>
-                </form>
-                <br/>
-                <br/>
-                <TodoList todoList={this.state.items}/>
-            </div>
+                    <div>
+                        <Route exact path="/" component={LoginView}/>
+                        <Route path="/todo" component={TodoAppView}/>
+                    </div>
+                </div>
+            </Router>
         );
     }
 
@@ -112,6 +100,8 @@ class App extends Component {
             dueDate: ''
         }));
     }
+
+  
 
 }
 
